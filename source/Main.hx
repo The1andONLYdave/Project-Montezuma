f3aa2221c4035e3eeabba7e993191756234a404a
David-Lee Kulsch <The1andONLYdave@googlemail.com>
2014-08-27 12:18:05 +0200

    TODO: adding correct scaling before v0.0.5
    scaling is a big issue, if we walk outside the window, even worse if you
    use a device like nex4 and walk some screens down, the whole leveldesign
    gets messed up. need a fix, or a force for immersive mode in newer
    android(?)

--- source/Main.hx
+++ source/Main.hx
@@ -9,11 +9,12 @@ import flixel.FlxGame;
 import flixel.FlxState;
 
 class Main extends Sprite 
-{
+{ //20x12 tiles = 320x192 pixel
 	var gameWidth:Int = 320; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 192; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = MenuState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+	//var zoom:Float = 1; // Testing for better scaling on different scrren sizes on android (nex4:1280x768(720 with softwarekeys on)(1,667) huawei-y300/s3mini:800x480(1,667))
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = false; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
@@ -54,7 +55,8 @@ class Main extends Sprite
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
 
-		if (zoom == -1)
+		if (zoom == -1) //TODO 27.8.14: add something like if ratio is not 1,667 make black borders around to archive this ratio, else we have devices with softwarekeys(onscreen), and due to scaling the game smaller (x-axis) we have something like 1 tilerow to much on y-axis.
+		
 		{
 			var ratioX:Float = stageWidth / gameWidth;
 			var ratioY:Float = stageHeight / gameHeight;
